6b734ad81dbac935fba301c36631615b5f658455
M.Gabała <82496006+MGabala@users.noreply.github.com>
2023-01-02 12:30:49 +0100

    update

--- CleanArchitectureDomain/Entities/CategoryEntity.cs
+++ CleanArchitectureDomain/Entities/CategoryEntity.cs
@@ -8,10 +8,10 @@ using CleanArchitectureDomain.Common;
 
 namespace CleanArchitectureDomain.Entities
 {
-    public class DatabaseRecordEntity : LogsEntity
+    public class CategoryEntity : LogsEntity
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string Description { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<EventEntity>? Events { get; set; }
     }
 }
