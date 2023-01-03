c0ef3345a2b3ee114f65ebbe0e1c1cfe8cbdce92
M.Gabała <82496006+MGabala@users.noreply.github.com>
2023-01-03 14:33:35 +0100

    update

---
+++ CleanArchitectureApplication/Repositories/Features/Events/Commands/UpdateEvent/UpdateEventCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+using CleanArchitectureApplication.Repositories.Persistance;
+
+using CleanArchitectureDomain.Entities;
+
+using MediatR;
+
+namespace CleanArchitectureApplication.Repositories.Features.Events.Commands.UpdateEvent
+{
+    public class UpdateEventCommandHandler : IRequestHandler<UpdateEventCommand>
+    {
+        private readonly IAsyncRepository<EventEntity> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateEventCommandHandler(IMapper mapper, IAsyncRepository<EventEntity> eventRepository)
+        {
+            _mapper = mapper;
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateEventCommand request, CancellationToken cancellationToken)
+        {
+            var eventToUpdate = await _eventRepository.GetByIdAsync(request.EventId);
+            _mapper.Map(request, eventToUpdate, typeof(UpdateEventCommand), typeof(EventEntity));
+            await _eventRepository.UpdateAsync(eventToUpdate);
+            return Unit.Value;
+        }
+    }
+}
