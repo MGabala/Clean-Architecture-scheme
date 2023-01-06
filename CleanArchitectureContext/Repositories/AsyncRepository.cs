f9889e6532cb548f1222d3fef2c6532a279133bf
M.Gabała <82496006+MGabala@users.noreply.github.com>
2023-01-06 16:50:15 +0100

    update

---
+++ CleanArchitectureContext/Repositories/AsyncRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CleanArchitectureApplication.Repositories.Persistance;
+
+namespace CleanArchitectureContext.Repositories
+{
+    public class AsyncRepository<T> : IAsyncRepository<T> where T : class
+    {
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
