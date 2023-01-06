283e9cbf009a73ef50aacbe29ba5eb4ef5120033
M.Gabała <82496006+MGabala@users.noreply.github.com>
2023-01-06 16:35:34 +0100

    update

---
+++ CleanArchitectureContext/MainDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitectureContext
+{
+    public class MainDatabase : DbContext
+    {
+        public MainDatabase(DbContextOptions<MainDatabase> options) : base(options)
+        {
+        }
+        
+    }
+}
