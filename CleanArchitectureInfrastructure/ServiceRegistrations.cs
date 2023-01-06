f82c6acdc6132d8b087f5e4757be9c4896d6187a
M.Gabała <82496006+MGabala@users.noreply.github.com>
2023-01-06 17:41:40 +0100

    update

---
+++ CleanArchitectureInfrastructure/ServiceRegistrations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CleanArchitectureApplication.Repositories.Persistance;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureInfrastructure
+{
+    public static class ServiceRegistrations
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //configure services for Infrastructure
+            return services;
+        }
+    }
+}
+
